ad943b364fa812f98fdd726b45837e956dd98b7b
Evgeny Kosmogradsky-Ivanov <kosmogradsky@gmail.com>
2020-01-18 23:07:36 +0100

    add hint cell type

--- web/main.dart
+++ web/main.dart
@@ -1,17 +1,78 @@
 import 'dart:html';
 import 'dart:math';
 
-class Cell {
+class CellCoords {
   final int row;
   final int column;
-  final bool isExplosive;
-  const Cell(this.row, this.column, this.isExplosive);
+  const CellCoords(this.row, this.column);
+}
+
+abstract class Cell {
+  int get row;
+  int get column;
+  
+  int get x => row * cellSize + 1;
+  int get y => column * cellSize + 1;
+
+  void renderContent(CanvasRenderingContext2D context);
+
+  void renderBorders(CanvasRenderingContext2D context) {
+    context.fillStyle = 'lightgray';
+    context.fillRect(
+      x,
+      y,
+      cellSize,
+      cellSize
+    );
+    context.strokeStyle = 'black';
+    context.lineWidth = 1;
+    context.strokeRect(
+      x,
+      y,
+      cellSize,
+      cellSize
+    );
+  }
+
+  void render(CanvasRenderingContext2D context) {
+    renderBorders(context);
+    renderContent(context);
+  }
 }
 
 const boardSize = 20;
 const cellSize = 40;
 const canvasSize = cellSize * boardSize + 2;
-const bombsCount = boardSize;
+const bombsCount = 60;
+
+class Hint with Cell {
+  final int row;
+  final int column;
+  final int bombsAround;
+  Hint(this.row, this.column, this.bombsAround);
+
+  void renderContent(CanvasRenderingContext2D context) {
+    if (bombsAround != 0) {
+      context.fillStyle = 'black';
+      context.font = '400 ${cellSize ~/ 1.3}px sans-serif';
+      context.fillText(bombsAround.toString(), x + cellSize * 0.25, y + cellSize * 0.75);
+    }
+  }
+}
+
+class Mine with Cell {
+  final int row;
+  final int column;
+  Mine(this.row, this.column);
+
+  void renderContent(CanvasRenderingContext2D context) {
+    context.fillStyle = 'gray';
+    context.beginPath();
+    context.arc(x + cellSize / 2, y + cellSize / 2, cellSize / 4, 0, 2 * pi);
+    context.fill();
+    context.stroke();
+  }
+}
 
 void main() {
   var pixelRatio = window.devicePixelRatio;
@@ -34,38 +95,47 @@ void main() {
     indicesWithBombs.add(randomizer.nextInt(cellsCount + 1));
   }
 
+  var coordinatesWithBombs = indicesWithBombs.map((index) {
+    var row = index ~/ boardSize;
+    var column = index % boardSize;
+
+    return CellCoords(row, column);
+  });
+
   var context = canvas.context2D;
   context.scale(scaleFactor, scaleFactor);
 
+  Cell cellGenerator(int index) {
+    var row = index ~/ boardSize;
+    var column = index % boardSize;
+
+    if (indicesWithBombs.contains(index)) {
+      return Mine(row, column);
+    }
+
+    var bombsAround = coordinatesWithBombs.fold<int>(
+      0,
+      (count, bomb) {
+        if ({row - 1, row, row + 1}.contains(bomb.row)) {
+          if ({column - 1, column, column + 1}.contains(bomb.column)) {
+            return count + 1;
+          }
+        }
+
+        return count;
+      }
+    );
+
+    return Hint(row, column, bombsAround);
+  }
+
   var cells = List.generate(
     cellsCount,
-    (index) => Cell(
-      index ~/ boardSize,
-      index % boardSize,
-      indicesWithBombs.contains(index)
-    )
+    cellGenerator
   );
 
   for (var cell in cells) {
-    var cellX = cell.row * cellSize + 1;
-    var cellY = cell.column * cellSize + 1;
-
-    context.strokeStyle = 'black';
-    context.lineWidth = 1;
-    context.strokeRect(
-      cellX,
-      cellY,
-      cellSize,
-      cellSize
-    );
-
-    if (cell.isExplosive) {
-      context.fillStyle = 'gray';
-      context.beginPath();
-      context.arc(cellX + cellSize / 2, cellY + cellSize / 2, cellSize / 4, 0, 2 * pi);
-      context.fill();
-      context.stroke();
-    }
+    cell.render(context);
   }
 
   querySelector('#output').append(canvas);
